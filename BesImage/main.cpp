b5c03b64c143b2a03342110f9aa187f83f068181
BensonLaur1995 <BensonLaur1995@gmail.com>
2018-06-28 20:39:58 +0800

    初始化项目

---
+++ BesImage/main.cpp
@@ -0,0 +1,61 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QCommandLineOption>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QCoreApplication::setApplicationVersion(QString("1.0.0.0"));
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Qt Dir View Example 【Qt目录查看例子】");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    QCommandLineOption dontUseCustomDirectoryIconsOption("c", "设置 QFileIconProvider::DontUseCustomDirectoryIcons");
+    parser.addOption(dontUseCustomDirectoryIconsOption);
+    parser.addPositionalArgument("directory", "The directory to start in.");
+    parser.process(app);
+    //const QString rootPath = parser.positionalArguments().isEmpty()
+    //    ? QString() : parser.positionalArguments().first();
+
+    MainWindow m;
+    m.show();
+
+//    QDirModel model;
+//    model.setFilter(QDir::AllDirs|QDir::NoDotAndDotDot|QDir::Files);
+
+//    QStringList nameFilters;
+//    nameFilters << "*.bmp" << "*.png";
+//    model.setNameFilters(nameFilters);
+
+//    model.iconProvider()->setOptions(QFileIconProvider::DontUseCustomDirectoryIcons);
+
+////    if (parser.isSet(dontUseCustomDirectoryIconsOption))
+////        model.iconProvider()->setOptions(QFileIconProvider::DontUseCustomDirectoryIcons);
+
+//    QTreeView fileTree;
+//    fileTree.setModel(&model);
+//    QString rootPath = "C:/";
+//    if (!rootPath.isEmpty()) {
+//        const QModelIndex rootIndex = model.index(QDir::cleanPath(rootPath));
+//        if (rootIndex.isValid())
+//            fileTree.setRootIndex(rootIndex);
+//    }
+
+//    // Demonstrating look and feel features
+//    fileTree.setAnimated(true);
+//    fileTree.setIndentation(15);
+//    fileTree.setSortingEnabled(true);
+//    //const QSize availableSize = QApplication::desktop()->availableGeometry(&tree).size();
+//    fileTree.resize(QSize(500,500));
+////    fileTree.setColumnWidth(0, 500*0.9);
+////    fileTree.setColumnHidden(1, true);
+////    fileTree.setColumnHidden(2, true);
+////    fileTree.setColumnHidden(3, true);
+
+//    fileTree.setWindowTitle(QObject::tr("Dir View"));
+//    fileTree.show();
+
+    return app.exec();
+}
---
+++ BesImage/mainwindow.h
@@ -0,0 +1,42 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QFileSystemModel>
+#include <QDirModel>
+#include <QFileIconProvider>
+#include <QTreeView>
+#include <QListWidget>
+#include <QAbstractScrollArea>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    void initUI();          //初始化UI
+
+    bool LoadFloder(QString DirPath); //载入文件夹
+
+public Q_SLOTS:
+    void showSelectedImg(const QModelIndex &index);
+
+
+    private:
+    QTreeView* fileTree;    //文件树
+    QListWidget *imageList; //缩略图标
+
+
+
+    QString lastResultMessage;                      //储存最后的结果信息
+};
+
+#endif // MAINWINDOW_H
